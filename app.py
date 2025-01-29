2308158098ab129b157605fd872e3807cee543da
CarlosFierros <carlosfierros250@gmail.com>
2025-01-29 10:50:13 -0700

    Iniciar programa gato y elaborar tablero.

--- app.py
+++ app.py
@@ -1,16 +1,40 @@
-# Calculo de coordenadas de lineas
-def calcular_y(x, m, b):
-    return m * x + b
-
-def main():
-    m = 2
-    b = 3
-    X = [x for x in range(1, 11)]
-    # Calcula Y elemento por elemento usando la función local calcular_y
-    Y = [calcular_y(x, m, b) for x in X]
-    print("Enteros: ")
-    coordenadas_enteros = list(zip(X, Y))
-    print(coordenadas_enteros)
-
+import argparse
+import matplotlib.pyplot as plt
+import funciones
+ 
+def main(m: float, b: float):
+ 
+    ''' Funcion principal que calcula las coordenadas de una linea recta
+    Recibimos: m y b
+    Regresa: nada '''
+ 
+    # m=2.0
+    # b=3.0
+    # X =[x for x in range(1,11)]
+    # Y =[funciones.Calcular_Y(x,m,b) for x in X]
+    # print('Enteros:')
+    # coordenadas_enteros = list(zip(X,Y))
+    # print(coordenadas_enteros)
+    plt.plot(X,Y)
+    plt.title(f'Linea con pendiente{m} y ordenada al origen {b}')
+    plt.xlabel('X')
+    plt.ylabel('Y')
+    plt.show()
+ 
+    X = [x/10.0 for x in range(10,110,5)]
+    Y = [funciones.Calcular_Y(x,m,b) for x in X]
+    coordenadas_flotantes = list(zip(X,Y))
+    print('Flotantes')
+    print(coordenadas_flotantes)
+    plt.plot(X,Y)
+    plt.show()
+ 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-m',type=float, help='Pendiente de la linea', default=2.0)
+    parser.add_argument('-b',type=float, help='Ordenada en el origen', default=3.0)
+    args = parser.parse_args()
+    main(args.m,args.b)
+ 
+    # main(m=2.0,b=3.0)
+    # :p
--- funciones.py
+++ funciones.py
@@ -1,17 +1,32 @@
-# Archivo con todas las funciones necesarias para la aplicacion "linea"
-
-def calcular_y(x, m, b):
-    return m*x + b
-
-def test_linea():
-    try:
-        assert calcular_y(0, 2, 3) == 3  # Verifica que la función funcione correctamente.
-        return True  # Si pasa el test, devuelve True.
-    except AssertionError:
-        return False  # Si falla el test, devuelve False.
-    
+def Calcular_Y(x: float,m: float ,b: float) -> float:
+    '''
+    Calcula el valor de y en una linea recta
+    x: valor de x
+    m: pendiente
+    b: interseccion de y
+    regresa el valor de y
+    '''
+    return m*x+b
+ 
+def main():
+    m=2
+    b=3
+    x=5
+    y= Calcular_Y(x,m,b)
+    print(f'Para x={x}, y={y}')
+ 
 if __name__ == '__main__':
-    if test_linea():
-        print("Todo bien")
+    main()
+ 
+def test_linea():
+    '''
+    Prueba de funcionamiento de calcular_Y
+    '''
+    y = Calcular_Y(0.0,2.0,3.0)
+    return y
+ 
+if __name__ == '__main__':
+    if test_linea() == 3.0:
+        print('everything aight')
     else:
-        print("Algo salio mal")
+        print('sum went wrong')
